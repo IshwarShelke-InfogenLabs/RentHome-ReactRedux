16f221d04ce57e45dd78d0a6b25d58004c5196b0
Ishwar <ishwar.shelke@infogen-labs.com>
2023-09-13 18:15:19 +0530

    fdsa

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuy-7n70WaZ3rkrxU_hlXJJiMq8qypGzc",
+  authDomain: "house-marketplace-app-3a5ee.firebaseapp.com",
+  projectId: "house-marketplace-app-3a5ee",
+  storageBucket: "house-marketplace-app-3a5ee.appspot.com",
+  messagingSenderId: "846818383300",
+  appId: "1:846818383300:web:0af68835578043089d8091",
+  measurementId: "G-4XHWHD6FCC",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
